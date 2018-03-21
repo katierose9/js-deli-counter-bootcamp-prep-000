50cd1733fd1bd25bc0969d61cac284bb352c558f
katierose9 <kwhitney98@gmail.com>
2018-03-21 21:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
-  for (var i = 0; i < katzDeliLine.length; i++) {
-        line.push(` ${i+1}. ${line[i]}`);
+  for (let i = 0; i < katzDeliLine.length; i++) {
+        line[i] = `${[i+1]}. ${katzDeliLine[i]}`;
   }
     return (`The line is currently:" ${line.join(', ')}`)
 };
